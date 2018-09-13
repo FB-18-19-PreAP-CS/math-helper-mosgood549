452cbe53f27ca960feeaef99901d2c25f8c673da
mosgood549 <marley549@icloud.com>
2018-09-13 14:11:25 -0500

    messed around a little bit

--- math_helper_osgoodm.py
+++ math_helper_osgoodm.py
@@ -1,2 +1,21 @@
+from math import *
 
-def 
+#function = input("What formula would you like to utilize today? ")
+
+def midpoint(x1,x2):
+    ''' returns the midpoint between two coordinates (x1,y1) and (x2,y2)
+    '''
+    mpoint = (x1+x2)/2
+
+#if input == "midpoint":
+#    in1 = input("What is your first point?")
+#    hellooo this is a change
+
+def main():
+    midpoint(2,1)
+    print(midpoint)
+    
+if __name__ == "__main__":
+    main()
+    
+    
