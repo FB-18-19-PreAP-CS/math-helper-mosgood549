6556fd3f96bf7b95bd36501f0e359f88e4594676
mosgood549 <marley549@icloud.com>
2018-09-19 14:10:44 -0500

    added volume of cylinder and cone doctests, also added midpoint value error

--- math_helper_osgoodm.py
+++ math_helper_osgoodm.py
@@ -3,7 +3,7 @@ from math import *
 #function = input("What formula would you like to utilize today? ")
 
 def midpoint(x1,y1,x2,y2):
-    ''' returns the midpoint between two coordinates (x1,y1) and (x2,y2)
+    ''' prints the midpoint between two coordinates (x1,y1) and (x2,y2)
     
     >>> midpoint(-3,3,5,3)
     (1.0,3.0)
@@ -14,7 +14,16 @@ def midpoint(x1,y1,x2,y2):
     >>> midpoint(-2,5,7,7)
     (2.5,6.0)
     
+    There cannot be a midpoint between the same points
+    >>> midpoint(2,3,2,3)
+    Traceback (most recent call last):
+        ...
+    ValueError: midpoint requires 2 different coordinates
+    
     '''
+    if x1 == x2 and y1 == y2:
+        raise ValueError("midpoint requires 2 different coordinates")
+    
     x = (x1+x2)/2
     y = (y1+y2)/2
     print("({},{})".format(x,y))
@@ -57,7 +66,76 @@ def herons_formula(s1,s2,s3):
     r_area = round(area, 3) 
     print(r_area)
     
+def v_cylinder(r,h):
+    '''prints the area for the volume of a cylinder, given the radius (r) and height(h)
+
+    >>> v_cylinder(4,10)
+    502.65
     
+    >>> v_cylinder(7,2)
+    307.88
+    
+    >>> v_cylinder(2.5,2.5)
+    49.09
+    
+    A cylinder can't have a negative radius length
+    >>> v_cylinder(-2, 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: the radius must be positive
+    
+    A cylinder cannot have a negative height length
+    >>> v_cylinder(8,-5)
+    Traceback (most recent call last):
+        ...
+    ValueError: the height must be positive
+    
+    '''
+    if r <= 0:
+        raise ValueError("the radius must be positive")
+    
+    if h <= 0:
+        raise ValueError("the height must be positive")
+        
+    volume_cyl = pi*((r**2)*h)
+    v_cyl_round = round(volume_cyl, 2)
+    print(v_cyl_round)
+    
+def v_cone(r, h):
+    '''prints the volume of a cone given the radius and the height
+
+    >>> v_cone(4,6)
+    100.53
+    
+    >>> v_cone(7,7)
+    359.19
+    
+    >>> v_cone(100,2)
+    20943.95
+    
+    A cone can't have a negative radius length
+    >>> v_cone(-20, 17)
+    Traceback (most recent call last):
+        ...
+    ValueError: the radius must be positive
+    
+    A cylinder cannot have a negative height length
+    >>> v_cone(40,-10)
+    Traceback (most recent call last):
+        ...
+    ValueError: the height must be positive
+    
+    
+    '''
+    if r <= 0:
+        raise ValueError("the radius must be positive")
+    
+    if h <= 0:
+        raise ValueError("the height must be positive")
+    
+    volume_cone = pi*((r**2)*(h/3))
+    vol_cone_round = round(volume_cone, 2)
+    print(vol_cone_round)
 
 #def main():
     
